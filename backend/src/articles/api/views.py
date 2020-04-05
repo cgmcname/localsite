5e0d485a0becf16052a3a0a27816c1e96a41226a
cgmcname <periclescgm@live.com>
2020-04-05 15:55:47 -0500

    Complete crud

--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,10 +1,30 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import viewsets
 
 from articles.models import Article
 from .serializers import ArticleSerializer
 
 
-class ArticleListView(ListAPIView):
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+
+
+
+""" from rest_framework.generics import ( 
+    CreateAPIView, 
+    ListAPIView, 
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+
+) """   
+
+""" class ArticleListView(ListAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
@@ -12,3 +32,15 @@ class ArticleListView(ListAPIView):
 class ArticleDetailView(RetrieveAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+class ArticleCreateView(CreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class ArticleUpdateView(UpdateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class ArticleDeleteView(DestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer """
--- frontend/gui/src/containers/ArticleDetailView.js
+++ frontend/gui/src/containers/ArticleDetailView.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import axios from 'axios';
 
-import { Card } from 'antd';
+import { useHistory } from "react-router-dom";
+
+import { Card, Button } from 'antd';
+
+import CustomForm from '../components/Form';
 
 
 
@@ -24,13 +28,38 @@ class ArticleDetail extends React.Component {
              });
 
     }
+
+    
+
+    handleDelete = (event) => {
+        const articleID = this.props.match.params.articleID;
+        axios.delete(`http://127.0.0.1:8000/api/${articleID}`)
+             .then(res => {window.location.reload();})
+             .catch(error => console.err(error));
+             
+        this.props.history.push("/");
+        
+            
+
+        
+
+    }
     
     
     render() {
         return (
-            <Card title={this.state.article.title}>
-                <p>{this.state.article.content}</p>
-            </Card>
+            <div>
+                <Card title={this.state.article.title}>
+                    <p>{this.state.article.content}</p>
+                </Card>
+                <CustomForm 
+                    requestType="put"
+                    articleID={this.props.match.params.articleID} 
+                    btnText="Update" />
+                <form onSubmitCapture={this.handleDelete}>
+                    <Button type="danger" htmlType="submit">Delete</Button>
+                </form>
+            </div>
         )
     }
 }
